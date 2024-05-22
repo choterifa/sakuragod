0025238d3afbadddbfbc50f20e4f0bdd2f1da089
Carlos E Valencia <choterifa@gmail.com>
2024-05-22 00:09:28 -0600

    limpieza archivos

--- static/Js/Audio.js
+++ static/Js/Audio.js
@@ -1,24 +1,8 @@
-var audio = document.getElementById("audio-fondo");
-var toggleAudio = document.getElementById("audio");
-var audioEncendido = true; // Inicialmente true porque el audio está reproduciéndose
-
-// Configurar el icono inicial
-toggleAudio.innerHTML = '<i class="bi bi-pause-fill"></i>';
-
-toggleAudio.addEventListener("click", function (event) {
-    event.preventDefault(); // Evitar la acción predeterminada del enlace
-
-    if (audioEncendido) {
-        audio.pause(); // Pausar el audio
-        toggleAudio.innerHTML = '<i class="bi bi-music-note-beamed"></i>'; // Cambiar a icono de play
-        audioEncendido = false;
-    } else {
-        audio.play(); // Reproducir el audio
-        toggleAudio.innerHTML = '<i class="bi bi-pause-fill"></i>'; // Cambiar a icono de pausa
-        audioEncendido = true;
-    }
-});
-
+// Obtener los elementos de audio y control de volumen
+const audio = document.getElementById('audio-fondo');
+const volumeControl = document.getElementById('volume');
+const toggleAudio = document.getElementById("audio");
+let audioEncendido = true; // Inicialmente true porque el audio está reproduciéndose
 
 // Lista de reproducción de audios
 const playlist = [
@@ -28,24 +12,52 @@ const playlist = [
 ];
 
 let currentTrack = 0;
-const audioElement = document.getElementById('audio-fondo');
 
 // Función para reproducir el siguiente audio en la lista de reproducción
 function playNextTrack() {
     currentTrack++;
     if (currentTrack < playlist.length) {
-        audioElement.src = playlist[currentTrack];
-        audioElement.play();
+        audio.src = playlist[currentTrack];
+        audio.play();
     } else {
         currentTrack = 0; // Reinicia la lista de reproducción
-        audioElement.src = playlist[currentTrack];
-        audioElement.play();
+        audio.src = playlist[currentTrack];
+        audio.play();
     }
 }
 
+// Comprobar si hay un volumen guardado en localStorage
+const savedVolume = localStorage.getItem('audioVolume');
+if (savedVolume !== null) {
+    audio.volume = savedVolume;
+    volumeControl.value = savedVolume;
+}
+
+// Añadir un event listener al control de volumen
+volumeControl.addEventListener('input', function () {
+    const newVolume = this.value;
+    audio.volume = newVolume;
+    localStorage.setItem('audioVolume', newVolume);
+});
+
+// Añadir un event listener al botón de toggle audio
+toggleAudio.addEventListener("click", function (event) {
+    event.preventDefault(); // Evitar la acción predeterminada del enlace
+
+    if (audioEncendido) {
+        audio.pause();
+        toggleAudio.innerHTML = '<i class="bi bi-music-note-beamed"></i>'; // Cambiar a icono de play
+        audioEncendido = false;
+    } else {
+        audio.play();
+        toggleAudio.innerHTML = '<i class="bi bi-pause-fill"></i>'; // Cambiar a icono de pausa
+        audioEncendido = true;
+    }
+});
+
 // Evento para cuando el audio termine de reproducirse
-audioElement.addEventListener('ended', playNextTrack);
+audio.addEventListener('ended', playNextTrack);
 
 // Iniciar la reproducción del primer audio
-audioElement.src = playlist[0];
-audioElement.play();
+audio.src = playlist[0];
+audio.play();
